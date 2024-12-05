074b48ea4166f796bd842220b0e0f6ee789b810c
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-12-05 18:55:40 -0500

    Segundo commit

--- tarea2/Program.cs
+++ tarea2/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Universidad Estatal Amazónica");
+﻿Console.WriteLine("Universidad Estatal Amazónica");
 Console.WriteLine();
 
 Rectangulo figura1 = new Rectangulo(2,3);
