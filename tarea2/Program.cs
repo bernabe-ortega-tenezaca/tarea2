b0ba021be8506c8970a5ca0ab699c691a7b1a837
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-12-05 18:45:47 -0500

    Definicion de objeto Rectangulo

--- tarea2/Program.cs
+++ tarea2/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Universidad Estatal Amazónica");
 Console.WriteLine();
+
+Rectangulo figura1 = new Rectangulo(2,3);
+Console.WriteLine(figura1.Area());
+
